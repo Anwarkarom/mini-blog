5e684d1d70ed2f8a303f7e503428b353491d3f10
Anwarkarom <anwarkarroum686@gmail.com>
2026-03-13 04:38:07 +0100

    test

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,4 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log("MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
-module.exports = app;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
